e6544762de61eb45e315da6c16f47595c6483309
LightningFlash <12lightningflash12@gmail.com>
2016-12-12 14:25:48 -0500

    Update randomCircles.js

--- randomCircles.js
+++ randomCircles.js
@@ -1,1 +1,20 @@
+//setup
+noStroke();
+background(0, 0, 0);
 
+//Loop
+var draw = function() {
+    //variables
+    var x = random(0, 400);
+    var y = random(0, 400);
+    var w = random(0, 40);
+    
+    //color variables
+    var r = random(0, 255);
+    var g = random(0, 255);
+    var b = random(0, 255);
+    
+    //Drawing circles
+    fill(r, g, b);
+    ellipse(x, y, w, w);
+};
